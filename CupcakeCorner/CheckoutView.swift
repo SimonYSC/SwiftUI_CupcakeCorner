60d2c8f11758a130684cd4eb50253fd6cc7e3fae
Simon Chao <ysc406@nyu.edu>
2021-08-25 16:02:08 -0400

    Complete the app.

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,70 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by YunShou Chao on 8/23/21.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                Image("cupcakes")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width)
+                
+                Text("Your total is: $\(self.order.cost, specifier: "%.2f")")
+                    .font(.title)
+                Button("Place order") {
+                    placeOrder()
+                }
+                .padding()
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        guard let encode = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encode
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity) \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }
+        .resume()
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
